037844225cf13fa8e8fa833f2570a5cc03219e6b
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-10 16:34:02 +0300

    declaring the winner

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ class Board extends React.Component { // parent component
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) return;
     squares[i] = this.state.xIsNext ? 'x' : 'o';
     this.setState({
       squares: squares,
@@ -42,7 +43,10 @@ class Board extends React.Component { // parent component
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) status = 'Winner: ' + winner;
+    else status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -89,3 +93,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 2, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
