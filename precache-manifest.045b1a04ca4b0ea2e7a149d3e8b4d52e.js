e829416ce01c28cb37235316d7790a51d4a928a5
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-14 00:18:42 +0300

    Updates

---
+++ precache-manifest.045b1a04ca4b0ea2e7a149d3e8b4d52e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473937ec2f5d08656f51dfccd19d2eda",
+    "url": "/sandbox/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b9a93e0a196781f1f934",
+    "url": "/sandbox/react-tic-tac-toe/static/css/main.b6177ed2.chunk.css"
+  },
+  {
+    "revision": "65dfd366a4da53ad9a3e",
+    "url": "/sandbox/react-tic-tac-toe/static/js/2.2a2a159d.chunk.js"
+  },
+  {
+    "revision": "b9a93e0a196781f1f934",
+    "url": "/sandbox/react-tic-tac-toe/static/js/main.d899cb7e.chunk.js"
+  },
+  {
+    "revision": "d2be12f835f94346e55d",
+    "url": "/sandbox/react-tic-tac-toe/static/js/runtime~main.1011cc52.js"
+  }
+]);
